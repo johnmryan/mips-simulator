a161cc4e01c1ee47778e5441f768d109fd6e7078
Luke <lgarriso@nd.edu>
2015-11-23 14:18:42 -0500

    Add part2

---
+++ part2/Cache.cpp
@@ -0,0 +1,34 @@
+/*
+ * DirectMappedCache.cpp
+ *
+ *  Created on: Oct 20, 2015
+ *      Author: jbb
+ */
+
+#include "Cache.h"
+
+#include "global.h"
+
+Cache::Cache(unsigned logBlockSize_, unsigned logDepth_)
+: logBlockSize(logBlockSize_),
+  logDepth(logDepth_),
+  accesses(0),
+  hits(0),
+  tagArray(new unsigned [1<<logDepth]),
+  validArray(new bool [1<<logDepth])
+{ }
+
+Cache::~Cache() {
+  delete tagArray;
+  delete validArray;
+}
+
+bool Cache::checkHit(unsigned addr) {
+  ++accesses;
+  ++hits;
+  return true;
+}
+
+void Cache::printStats() {
+  printf("accesses = %u  hits = %u  hit rate = %f\n", accesses, hits, (float) hits/accesses);
+}
